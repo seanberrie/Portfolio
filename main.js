e4ceb3e2b3ceadaf01ffbe68b05bf4f55b4ee08b
Sean Berrie <seanberrie21@gmail.com>
2018-11-30 09:54:46 -0800

    added fading testimonal sticky nav completed and jquery carousel background started still need to fix this and anchor links

--- main.js
+++ main.js
@@ -4,4 +4,47 @@ var $email = $('#email');
 
 $email.on('click',function(){
     alert("Email: SeanBerrie21@gmail.com");
-})
+});
+
+
+var num = 800;
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.nav').addClass('sticky');
+    } else {
+        $('.nav').removeClass('sticky');
+    }
+});
+
+
+var terms = ["here is test one.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus ab facere natus voluptatum laudantium totam commodi eius magnam sapiente, aperiam ipsam blanditiis accusamus sed adipisci assumenda amet ratione voluptates similique.",
+            "this is 2, ipsum dolor sit amet consectetur adipisicing elit. Delectus ab facere natus voluptatum laudantium totam commodi eius magnam sapiente, aperiam ipsam blanditiis accusamus sed adipisci assumenda amet ratione voluptates similique.",
+            "we go for 3, ipsum dolor sit amet consectetur adipisicing elit. Delectus ab facere natus voluptatum laudantium totam commodi eius magnam sapiente, aperiam ipsam blanditiis accusamus sed adipisci assumenda amet ratione voluptates similique.",];
+
+function rotateTerm() {
+  var phrase = $(".test").data("term") || 0;
+  $(".test").data("term", phrase == terms.length -1 ? 0 : phrase + 1).text(terms[phrase]).fadeIn()
+              .delay(5000).fadeOut(500, rotateTerm);
+};
+$(rotateTerm);
+
+!function(t){
+    "use strict";
+    var a=t("[data-slides]"),ni=0,s=a.data("slides"),e=s.length,
+    n=function(){
+        if(ni>=a.length){ni=0}
+        else{ni++};
+        a.css("background-image",'url("'+s[ni]+'")').show(0,
+            function(){setTimeout(n,5e3
+                )})};
+    n()}
+    
+    (jQuery);
+
+
+
+
+
+
+// https://i.imgur.com/xIGYFTZ.jpg,https://i.imgur.com/NZUsoJR.jpg?1,https://i.imgur.com/OsarbSu.jpg,https://i.imgur.com/YhwtxmW.jpg
