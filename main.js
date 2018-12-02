5ae2f8f4a809473b0d9adb96cf9724bc34199533
Sean Berrie <seanberrie21@gmail.com>
2018-12-02 14:36:28 -0800

    fixed test, box small margin errors, added media query, and hamburger box added, jquery toggle class

--- main.js
+++ main.js
@@ -22,21 +22,33 @@ $(window).bind('scroll', function () {
     }
 });
 
+/////////
+/////////  media query nav bar
+/////////
+var num = 800;
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.phonecont').addClass('stickyphone');
+    } else {
+        $('.phonecont').removeClass('stickyphone');
+    }
+});
+
 
 /////////
 /////////  Testimonial loop
 /////////
 var phrase = $('.test')
-var x = 1;
-var terms = ["here is test one.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus ab facere natus voluptatum laudantium totam commodi eius magnam sapiente, aperiam ipsam blanditiis accusamus sed adipisci assumenda amet ratione voluptates similique.",
+var x = 0;
+var terms = ["Sean Berrie is a hardworking guy who does whatever is asked of him. While on a trip to a UFO conference in Joshua Tree he took one for the team and slept in the car while his tubby friend had sex with a local. Sean has a strong work ethic and is great at finding ways to get the task done, fast and effectively. He really loves Reddit and spends way too much time trying to educate his flat earther friends on Facebook. If I had to say which vegetable Sean is I wound say he's a carrot because he's long and skinny and full of good things for you. Sean was also voted the most handsome man in Utica, NY in 2002.",
             "this is 2, ipsum dolor sit amet consectetur adipisicing elit. Delectus ab facere natus voluptatum laudantium totam commodi eius magnam sapiente, aperiam ipsam blanditiis accusamus sed adipisci assumenda amet ratione voluptates similique.",
             "we go for 3, ipsum dolor sit amet consectetur adipisicing elit. Delectus ab facere natus voluptatum laudantium totam commodi eius magnam sapiente, aperiam ipsam blanditiis accusamus sed adipisci assumenda amet ratione voluptates similique.",
         ];
 function rotateTerm() {
-    phrase.text(terms[x % 4]).fadeIn(1000).delay(4000).fadeOut(1000)
+    phrase.text(terms[x % 3]).fadeIn(1000).delay(4000).fadeOut(1000)
     x++;
 };
-
+rotateTerm();
 setInterval(rotateTerm, 6000)
 
 
@@ -55,3 +67,18 @@ var slideshow = function(){
 };
 
 setInterval(slideshow,4000);
+
+/////////
+/////////  click icon nav
+/////////
+
+var $icon = $(".icon")
+var $nav = $(".nav")
+
+$icon.click(function(){
+    if (!$nav.hasClass("navopen")){
+        $nav.addClass("navopen");
+    } else {$nav.removeClass("navopen")}
+});
+
+
